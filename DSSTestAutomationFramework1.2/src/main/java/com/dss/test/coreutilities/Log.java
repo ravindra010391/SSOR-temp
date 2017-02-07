3b2658a0ebf0bc1f95abf68ca831a4d06995ab2e
ravindra010391 <ravindra.manadage@gmail.com>
2017-02-07 22:30:02 +0530

    fgh

--- DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Log.java
+++ DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Log.java
@@ -12,7 +12,7 @@ public class Log {
 	
 	public static void logInit(){
 		//BasicConfigurator.configure();
-		//PropertyConfigurator.configure("/DSS-SSORTestAutomation/src/main/java/log4j.properties");
+		PropertyConfigurator.configure(System.getProperty("user.dir")+"\\src\\main\\java\\log4j.properties");
 		//CONSOLE_LOGGER = Logger.getLogger("CONSOLE_LOGGER");
 		Log = Logger.getLogger("FILE_LOGGER");
 	}
