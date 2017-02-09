770c559fe2a4dd51ffe7300fddb615ac14fe85d8
ravindra010391 <ravindra.manadage@gmail.com>
2017-02-09 18:47:36 +0530

    d

--- DSSTestAutomationFramework1.2/src/main/java/com/dss/test/apputilities/xmlConfig.java
+++ DSSTestAutomationFramework1.2/src/main/java/com/dss/test/apputilities/xmlConfig.java
@@ -5,12 +5,71 @@ public class xmlConfig {
 	public static void createXMLFile(String marketConfigData){
 		
 		String[] marketConfig = marketConfigData.split("-");
-		String marketName = marketConfig[0];
-		String marketEnvironment = marketConfig[1];
-		String allBrowsers = marketConfig[2];
-		System.out.println(marketName+" "+marketEnvironment+" "+allBrowsers);
+		String marketToken = marketConfig[0];
+		String marketEnvironmentToken = marketConfig[1];
+		String allBrowsersTocken = marketConfig[2];
+		
+		System.out.println(marketToken+" "+marketEnvironmentToken+" "+allBrowsersTocken);
+		String marketFullName = getMarketFullName(marketToken);
+		String URL = createURL(marketFullName, marketEnvironmentToken);
+		getAllBrowser(allBrowsersTocken);
 		
 	}
 	
+	
+	
+	
+	private static String createURL(String marketFullName,String marketEnvironmentToken){
+		String URL = null;
+		
+		if(marketEnvironmentToken.equalsIgnoreCase("st")){
+			URL = "http://ngux."+marketFullName+".stage.tribdev.com/";
+		}
+		if(marketEnvironmentToken.equalsIgnoreCase("pr")){
+			URL = "http://www."+marketFullName+".com/";
+		}
+		
+		return URL;
+}
+	
+	private static String getMarketFullName(String marketNameToken){
+		
+		String marketFullName = null;
+		switch (marketNameToken.toLowerCase()) {
+		
+		case "la": marketFullName = "latimes"; break;
+		case "ct": marketFullName = "chicagotribune"; break;
+		case "ss": marketFullName = "sun-sentinel"; break;
+		case "os": marketFullName = "orlandosentinel"; break;
+		case "hc": marketFullName = "courant"; break;
+		case "dp": marketFullName = "dailypress"; break;
+		case "bs": marketFullName = "baltimoresun"; break;
+		case "mcall": marketFullName = "mcall"; break;
+		case "cct": marketFullName = "carrollcountytimes"; break;
+		case "acg": marketFullName = "capitalgazette"; break;
+		case "vg": marketFullName = "vagazette"; break;
+		case "sdut": marketFullName = "sandiegouniontribune"; break;
 
+		default:
+			System.out.println("Please enter valid MARKET name!!!");
+			break;
+		}
+		return marketFullName;
+	}
+
+	private static String[] getAllBrowser(String browserToken){
+		String[] browsers = null;
+		
+		if(browserToken.length()>=4){
+			for(int i = 0;i<browserToken.length(); i++ ){
+				browsers[i] = browserToken.substring(i, i+1);
+				System.out.println(browsers[i]);
+				i=i+2;
+			}
+		}else{
+			browsers[0] = browserToken;
+		}
+		return browsers;
+	}	
+	
 }
