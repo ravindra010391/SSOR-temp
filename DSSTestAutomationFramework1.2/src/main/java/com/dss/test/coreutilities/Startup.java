5899bb6560c4a89f7e42d4dc07bce2e5f5dd4428
ravindra010391 <ravindra.manadage@gmail.com>
2017-02-15 12:04:40 +0530

    a

--- DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Startup.java
+++ DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Startup.java
@@ -4,6 +4,9 @@ public class Startup {
 	
 	public static void main(String[] args) {
 		System.out.println("This is the main method");
+		
+		String confi_para = System.getProperty("CONFIG_PARAMETER");
+		System.out.println("Config parameter : " +confi_para);
 	}
 
 }
