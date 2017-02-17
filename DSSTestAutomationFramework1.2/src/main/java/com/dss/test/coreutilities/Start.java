f76c908b6ca1b4e8ca476b097bc32c56fcece053
ravindra010391 <ravindra.manadage@gmail.com>
2017-02-17 19:52:09 +0530

    ds

--- DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Start.java
+++ DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Start.java
@@ -1,10 +1,11 @@
 package com.dss.test.coreutilities;
 
+import com.dss.test.isologin.demoTest;
 import com.dss.test.runner.Runner;
 
 public class Start {
 
-	public static void main(String[] args) throws Exception {
+	public  void mainTe(String string) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("this is start");
 		System.out.println("this is start");
@@ -14,6 +15,8 @@ public class Start {
 		//Runner r = new Runner();
 		//r.mainTest();
 	
+		demoTest d = new demoTest();
+		d.demoTest();
 	}
 
 }
