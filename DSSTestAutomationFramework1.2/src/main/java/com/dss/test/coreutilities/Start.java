5818fbdc9e9c6709dc919a753f00ce7ea602adda
ravindra010391 <ravindra.manadage@gmail.com>
2017-02-17 14:51:20 +0530

    frtrt

---
+++ DSSTestAutomationFramework1.2/src/main/java/com/dss/test/coreutilities/Start.java
@@ -0,0 +1,19 @@
+package com.dss.test.coreutilities;
+
+import com.dss.test.runner.Runner;
+
+public class Start {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("this is start");
+		System.out.println("this is start");
+		
+		System.out.println("this is start");
+		
+		//Runner r = new Runner();
+		//r.mainTest();
+	
+	}
+
+}
